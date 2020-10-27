ff9484280a2f7ef293fb8a8a82066a80e40fda9f
mcarlomagno <marcoscarlomagno1@gmail.com>
2020-10-27 09:44:44 -0300

    feat: footer content

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,7 +1,30 @@
+import './footer.css';
+
 function Portfolio () {
     return (
-        <div>
-            My awesome footer
+        <div className="footer-container">
+            <div>
+                <div>
+                    About me
+                </div>
+                <div>
+                    TODO: Text introducing myself
+                </div>
+                <div>
+                    Social media
+                </div>
+                <div>
+                    TODO: Social Media Icons
+                </div>
+            </div>
+            <div>
+                <div>
+                    Contact me
+                </div>
+                <div>
+                    TODO: Contact form
+                </div>
+            </div>
         </div>
     );
 }
