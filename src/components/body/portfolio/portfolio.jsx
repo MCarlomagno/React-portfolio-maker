89e393b093c88222f7b26e4da75a76705f25d534
mcarlomagno <marcoscarlomagno1@gmail.com>
2020-10-26 21:10:31 -0300

    feat: item image and structure

--- src/components/body/portfolio/portfolio.jsx
+++ src/components/body/portfolio/portfolio.jsx
@@ -1,13 +1,14 @@
 import PortfolioItem from './portfolio-item/portfolio-item'
 import './portfolio.css'
+import imageUrl from './../../../assets/toolbar-image.jpg';
 
 function Portfolio () {
     return (
             <div className="portfolio-container">
-                <div className="portfolio-element"><PortfolioItem title={'title 1'}/></div>
-                <div className="portfolio-element"><PortfolioItem title={'title 2'}/></div>
-                <div className="portfolio-element"><PortfolioItem title={'title 3'}/></div>
-                <div className="portfolio-element"><PortfolioItem title={'title 4'}/></div>
+                <div className="portfolio-element"><PortfolioItem title={'title 1'} description={'description 1'} image={imageUrl}/></div>
+                <div className="portfolio-element"><PortfolioItem title={'title 2'} description={'description 2'} image={imageUrl}/></div>
+                <div className="portfolio-element"><PortfolioItem title={'title 3'} description={'description 3'} image={imageUrl}/></div>
+                <div className="portfolio-element"><PortfolioItem title={'title 4'} description={'description 4'} image={imageUrl}/></div>
             </div>
     );
 }
