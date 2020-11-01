34b2f077b18d0f5dd37645bbb887f07ff7acaad4
mcarlomagno <marcoscarlomagno1@gmail.com>
2020-11-01 20:26:53 -0300

    refactor: migration from jsx to tsx

---
+++ src/components/body/body.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Portfolio from './portfolio/portfolio';
+
+class Body extends React.Component {
+    render() {
+        return (
+            <Portfolio />
+        );
+    }
+}
+
+export default Body;
---
+++ src/components/toolbar/name/name.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+class Name extends React.Component {
+
+    render() {
+        return (
+            <p>Marcos Carlomagno</p>
+        );
+    }
+
+}
+
+export default Name;
