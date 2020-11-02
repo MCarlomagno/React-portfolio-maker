3faeb5ff6d7b1866659b3dcf8d72435f8c04e892
mcarlomagno <marcoscarlomagno1@gmail.com>
2020-11-01 21:59:32 -0300

    feature: show pop up on click

--- src/components/body/portfolio/portfolio.tsx
+++ src/components/body/portfolio/portfolio.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './portfolio.css';
 import PortfolioItem from './portfolio-item/portfolio-item';
 import imageUrl from './../../../assets/toolbar-image.jpg';
 
+interface IPortfolioProps {
+}
 
-class Portfolio extends React.Component {
+interface IPortfolioState {
+}
+
+class Portfolio extends Component<IPortfolioProps, IPortfolioState> {
+
     render() {
         const fakeDescription = 'Lorem  asdasd ipsum asdasd asd asd asd asd asdas dasd as das d asd asd asdna asdf adf asdfasf asdfas dfas d asdfa sdfa sdf adsf dolor sit amet consectetur, adipisicing elit. Delectus numquam maiores iure labore ad, blanditiis tempore quod sunt? Distinctio eum repudiandae esse culpa praesentium voluptatibus odit perspiciatis! Iusto maxime ex vitae numquam eos ratione tempora quis fugit nulla, rerum quisquam atque, quo provident inventore recusandae modi ipsam, facilis quas explicabo?';
         const fakeTitle1 = 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus, sunt?'
